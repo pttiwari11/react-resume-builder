fc64d2b5d0fe8923976a39c77c022e38c4783e22
Pushpendra Tiwari <78895037+pttiwari11@users.noreply.github.com>
2023-04-13 16:48:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Form from "./components/Form";
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">Let's generate your Resume!</h1>
+      <Form />
+    </div>
+  );
+}
